c5efbb838f1de8900d34681a42d7a0dce94b67d9
xingyazhou <xingyaz@gmail.com>
2020-05-24 15:52:29 +0200

    updated according to review feedback

--- sql_queries.py
+++ sql_queries.py
@@ -12,72 +12,83 @@ time_table_drop = "DROP table  IF EXISTS time"
 # Since this is a subset of the much larger dataset, the sub dataset will only have 1 row with values for value containing ID 
 # for both song_id and artist_id in the fact table. Those are the only 2 values that the query in the sql_queries.py 
 # will return that are not-NONE. The rest of the rows will have NONE values for those two variables.
+
 songplay_table_create = ("""create table if not exists 
     songplays
     (
-    songplay_id SERIAL,
-    start_time varchar NOT NULL, 
-    user_id int NOT NULL,
-    song_id varchar,
-    artist_id varchar,
-    level varchar,
-    session_id int NOT NULL,
-    location varchar,
-    user_agent varchar,
-    PRIMARY KEY (start_time, user_id)        
+        songplay_id SERIAL PRIMARY KEY,
+        start_time varchar NOT NULL, 
+        user_id int NOT NULL ,
+        song_id varchar ,
+        artist_id varchar,
+        level varchar,
+        session_id int NOT NULL,
+        location varchar,
+        user_agent varchar 
     )
 """)
 
 user_table_create = ("""create table if not exists 
     users
     (
-    user_id int PRIMARY KEY, 
-    first_name varchar, 
-    last_name varchar, 
-    gender varchar,
-    level varchar
+        user_id int PRIMARY KEY, 
+        first_name varchar, 
+        last_name varchar, 
+        gender varchar,
+        level varchar
     )
 """)
 
 song_table_create = ("""create table if not exists 
     songs
     (
-    song_id varchar PRIMARY KEY,
-    title varchar,
-    artist_id varchar NOT NULL, 
-    year int,
-    duration float
+        song_id varchar PRIMARY KEY,
+        title varchar,
+        artist_id varchar NOT NULL, 
+        year int,
+        duration float
     )
 """)
 
 artist_table_create = ("""create table if not exists 
     artists
     (
-    artist_id varchar PRIMARY KEY, 
-    name varchar, 
-    location varchar, 
-    latitude float, 
-    longitude float
+        artist_id varchar PRIMARY KEY, 
+        name varchar, 
+        location varchar, 
+        latitude float, 
+        longitude float
     )
 """)
 
-time_table_create = ("""create table if not exists time(start_time varchar, hour int, day int,week int, month int, year int,weekday int)
+time_table_create = ("""create table if not exists 
+    time
+    (
+        start_time varchar PRIMARY KEY, 
+        hour int, 
+        day int,
+        week int, 
+        month int, 
+        year int,
+        weekday int
+    )
 """)
+
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""INSERT INTO 
     songplays
     (
-    songplay_id,
-    start_time,
-    user_id,
-    level,
-    song_id,
-    artist_id,
-    session_id,
-    location,
-    user_agent
+        songplay_id,
+        start_time,
+        user_id,
+        level,
+        song_id,
+        artist_id,
+        session_id,
+        location,
+        user_agent
     )
     VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
     ON CONFLICT DO NOTHING
@@ -86,11 +97,11 @@ songplay_table_insert = ("""INSERT INTO
 user_table_insert = ("""INSERT INTO 
     users
     (
-    user_id,
-    first_name,
-    last_name,
-    gender,
-    level
+        user_id,
+        first_name,
+        last_name,
+        gender,
+        level
     )
     VALUES (%s,%s,%s,%s,%s)
     ON CONFLICT (user_id)
@@ -101,11 +112,11 @@ song_table_insert =  ("""
     INSERT INTO 
     songs
     (
-    song_id, 
-    title, 
-    artist_id, 
-    year, 
-    duration
+        song_id, 
+        title, 
+        artist_id, 
+        year, 
+        duration
     )
     VALUES (%s, %s, %s, %s, %s)
     ON CONFLICT DO NOTHING;
@@ -116,11 +127,11 @@ artist_table_insert = ("""
     INSERT INTO 
     artists
     (
-    artist_id, 
-    name, 
-    location, 
-    latitude, 
-    longitude
+        artist_id, 
+        name, 
+        location, 
+        latitude, 
+        longitude
     )
     VALUES (%s, %s, %s, %s, %s)
     ON CONFLICT DO NOTHING;
@@ -131,13 +142,13 @@ time_table_insert = ("""
     INSERT INTO 
     time
     (
-    start_time,
-    hour,
-    day,
-    week,
-    month,
-    year,
-    weekday
+        start_time,
+        hour,
+        day,
+        week,
+        month,
+        year,
+        weekday
     )
     VALUES(%s, %s, %s, %s, %s, %s, %s)
     ON CONFLICT DO NOTHING
